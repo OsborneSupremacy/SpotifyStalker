f323e70524be5f216c8b2b7d8382366e03d378c0
Ben Osborne <osborne.ben@gmail.com>
2021-01-27 07:33:48 -0600

    updates to random pickers

--- SpotifyStalker.Interface/IRandomProvider.cs
+++ SpotifyStalker.Interface/IRandomProvider.cs
@@ -4,9 +4,13 @@ namespace SpotifyStalker.Interface
 {
     public interface IRandomProvider
     {
+        public string GetPersonName();
+
         public string GetWord();
 
         public string GetLocation();
+
+        public string GetGenre();
 
         T PickRandom<T>(IEnumerable<T> items);
     }
