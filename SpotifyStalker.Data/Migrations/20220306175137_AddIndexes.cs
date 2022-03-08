5e3da8a53681b5f15725f9648d5234021a6852a5
Ben Osborne <72934502+ben-osborne-intelliscript@users.noreply.github.com>
2022-03-08 09:41:37 -0600

    added indexes. don't re-query artist's tracks that have already been queried. (#12)
    * Create launchSettings.json
    
    * added `AddedDate` to Track
    
    * added indexes. added functionality to not re-query tracks for artists already queried.

---
+++ SpotifyStalker.Data/Migrations/20220306175137_AddIndexes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotifyStalker.Data.Migrations
+{
+    public partial class AddIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracks_Name",
+                table: "Tracks",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artists_ArtistName",
+                table: "Artists",
+                column: "ArtistName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tracks_Name",
+                table: "Tracks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Artists_ArtistName",
+                table: "Artists");
+        }
+    }
+}
