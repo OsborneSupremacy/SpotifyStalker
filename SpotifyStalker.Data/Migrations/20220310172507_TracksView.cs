ec8b500840f18ee95cfa9ce49f9f96c767b2f58c
Ben Osborne <ben.osborne@milliman.com>
2022-03-10 11:29:06 -0600

    added Tracks view

---
+++ SpotifyStalker.Data/Migrations/20220310172507_TracksView.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotifyStalker.Data.Migrations
+{
+    public partial class TracksView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+
+create or alter view dbo.vTracks as
+
+select
+    a.ArtistId,
+    a.ArtistName,
+    a.Genres,
+    t.id,
+    t.[Name],
+    t.Popularity,
+    t.Danceability,
+    t.Energy,
+    t.[Key],
+    t.Loudness,
+    t.Mode,
+    t.Speechiness,
+    t.Acousticness,
+    t.Instrumentalness,
+    t.Liveness,
+    t.Valence,
+    t.Tempo,
+    t.DurationMs,
+    t.TimeSignature,
+    t.AddedDate
+from
+    dbo.Artists a
+    inner join dbo.Tracks t on a.ArtistId = t.ArtistId;
+
+");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop view if exists dbo.vTracks;");
+        }
+    }
+}
