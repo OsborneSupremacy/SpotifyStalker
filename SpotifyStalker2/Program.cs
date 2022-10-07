f358c32fbb1acd625848c2a2253fddf964afb791
Ben Osborne <osborne.ben@gmail.com>
2022-10-07 09:19:47 -0500

    Updated Serilog convention

--- SpotifyStalker2/Program.cs
+++ SpotifyStalker2/Program.cs
@@ -35,10 +35,10 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .UseSerilog()
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder
-                    .UseSerilog()
                     .UseStartup<Startup>();
             });
 }
